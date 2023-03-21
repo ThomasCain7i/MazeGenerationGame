397826dda66fd9dee8c97ec0924ddda2efebeb87
unknown <Thomas@LON-C7-7968.students.local>
2023-03-21 17:16:37 +0000

    Player Ball and maze start

---
+++ Assets/Scripts/BallCotroller.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCotroller : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            rigid.AddForce(Vector3.right * speed);
+        }
+        else if(Input.GetAxis("Horizontal") < 0)
+        {
+            rigid.AddForce(-Vector3.right * speed);
+        }
+
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            rigid.AddForce(-Vector3.forward * speed);
+        }
+    }
+}
--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -1,35 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MazeGenerator : MonoBehaviour
 {
-    public GameObject Wall; //Reference to the wall prefab
-    public int mazeSize = 15; //Size of the maze
-    public float perlinThreshold = 0.5f; // threshold value for Perlin noise
-    public float startPointThreshold = 0.1f;
-    public float endPointThreshold = 0.9f;
+    public GameObject wallPrefab; // reference to the cube wall prefab
+    public int mazeSize = 15; // size of the maze
+    public float perlinThreshold; // threshold value for Perlin noise
+    public float startPointThreshold = 0.1f; // threshold value for start point
+    public float endPointThreshold = 0.9f; // threshold value for end point
 
-    public void Start()
+    public bool wallHere = false;
+
+    public GameObject startPoint; // reference to the start point object
+
+    void Start()
     {
+        perlinThreshold = Random.Range(0.1f, 0.9f);
+        // generate maze walls
         for (int i = 0; i < mazeSize; i++)
         {
-            float perlinValue = Mathf.PerlinNoise(i / (float)mazeSize, i / (float)mazeSize);
-            //Create a for loop that will iterate over the rows of the maze.
-            if (perlinValue < perlinThreshold)
+            for (int j = 0; j < mazeSize; j++)
             {
-                GameObject newWall = Instantiate(Wall, new Vector3(i, 0, i), Quaternion.identity);
-                newWall.transform.parent = transform;
+                
+                // generate Perlin noise value for current cell
+                float perlinValue = Mathf.PerlinNoise(i / (float)mazeSize, j / (float)mazeSize);
+                
+                // instantiate a new wall prefab at the current position if Perlin noise value is below threshold
+                if (perlinValue < perlinThreshold)
+                {
+                    GameObject newWall = Instantiate(wallPrefab, new Vector3(i, 0, j), Quaternion.identity);
+                    newWall.transform.parent = transform;
+                }
             }
         }
-        for (int j = 0; j < mazeSize; j++)
-        {
-
-        }
-    }
-
-    public void Update()
-    {
-        transform.localScale = new Vector3(15, 15, 15);
     }
 }
+
+/*
+No idea what the perlin noise does (i get it effects how the walls are instantiated just dont understand how)
+Creates a 15 - 15 grid of walls at the original rotation
+*/
+
+
+/*
+It seems that you are creating the start point object inside the loop that generates the maze. 
+This means that every time a cell with a Perlin noise value below the start point threshold is found, a new start point object will be created, which is not what you want.
+To fix this issue, you can move the creation of the start point object outside of the loop that generates the maze, 
+ and use the startPoint variable to store a reference to the start point object. 
+Here's the modified code ^
+With this modification, the script should generate the maze walls and create only one start point object when a cell with a Perlin noise value below the start point threshold is found.
+*/
